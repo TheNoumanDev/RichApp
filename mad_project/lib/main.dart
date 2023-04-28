a9f5e9e0d86cd54159ff317dad979a85206cf3f0
TheNoumanDev <m_nouman@hotmail.com>
2023-04-28 15:24:30 +0500

    Create Basic File Structure

---
+++ mad_project/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mad_project/Home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
